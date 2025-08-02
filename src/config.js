f7d8a2301da5f14a672e96c3be26510ee4992eab
Ravi <ravirana75036@gmail.com>
2025-08-02 14:28:54 +0530

    asdf

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// export const apiHost ='https://backendblood.game4.live';
+// export const apiHost ='https://backendblood.game4.live';aadfm.sf
 // export const apiHost = 'http://localhost:7675';
 export const apiHost = 'https://anemoscan.backend.healthinnovations.in';
 
