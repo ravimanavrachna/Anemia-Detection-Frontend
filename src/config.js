2c4f6e02b0a9ce8b7344d7f6526a0b1be11ba5f2
Ashwani Kumar <ashwani1609200@gmail.com>
2025-08-05 09:52:14 +0530

    added something

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // export const apiHost ='https://backendblood.game4.live';aadfm.sf
 // export const apiHost = 'http://localhost:1010';
-export const apiHost = 'https://anemoscan.backend.healthinnovations.in'
+export const apiHost = 'https://anemoscan.backend.healthinnovations.in' 
+export const editsomething="editsomething"
 
 
 
